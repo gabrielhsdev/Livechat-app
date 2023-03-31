fae274943cccba84f75e6a12694b1e2258bc2c89
gabriel <henriquegabrielhs21@gmail.com>
2023-03-31 00:43:29 -0300

    structure changes, redirecting pages, DB push work

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,11 @@
 import * as React from 'react'
 import { ChakraProvider, Flex } from '@chakra-ui/react'
-import Messageboard from './src/messageboard/Messageboard'
-import Login_main from './src/login/Login_main'
-import Regster_main from './src/register/Register_main'
-import mergedTheme from '../styles/customTheme'
+import Messageboard from './Messageboard'
+import Login_main from './Login_main'
+import Register_main from './Register_main'
 
 export default function Home() {
   return (
-    <ChakraProvider theme={mergedTheme}>
-      <Flex className='outerboard_class' bgColor='brand.300'>
-        <Regster_main />
-      </Flex>
-    </ChakraProvider>
+    <Login_main />
   )
 }
